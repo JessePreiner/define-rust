8e892539570b728c0d7d4caba74dca457a4f556a
Jesse Preiner <jesse.preiner@yardi.com>
2018-02-09 15:46:14 -0600

    adds https support via connector + adds config management, api info in untracked toml

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,50 @@
+//#![deny(warnings)]
 extern crate futures;
 extern crate hyper;
+extern crate hyper_tls;
 extern crate tokio_core;
+extern crate pretty_env_logger;
+extern crate config;
+extern crate serde;
 
+#[macro_use]
+extern crate serde_derive;
+
+mod settings;
+
+use settings::Settings;
 use std::io::{self, Write};
+use std::env;
 use futures::{Future, Stream};
 use hyper::Client;
+use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
 
 fn main() {
-  pretty_env_logger::init();
-
+	let settings = Settings::new();
+	println!("{:?}", settings);
 	let url = match env::args().nth(1) {
 	  Some(url) => url,
 		None => {
 			println!("Usage: client <url>");
-				return;
-				}
+			return;
+		}
 	};
 
 	let url = url.parse::<hyper::Uri>().unwrap();
-	if url.scheme() != Some("http") {
-		println!("This example only works with 'http' URLs.");
-		return;
-	}
+	let mut core = Core::new().unwrap();
+	let client = Client::configure()
+		.connector(HttpsConnector::new(4, &core.handle()).unwrap())
+		.build(&core.handle());
+
+	let work = client.get(url).and_then(|res| {
+		println!("Response: {}", res.status());
+		res.body().for_each(|chunk| {
+			io::stdout()
+			.write_all(&chunk)
+			.map_err(From::from)
+		})
+	});
+
+	core.run(work).unwrap();
 }
